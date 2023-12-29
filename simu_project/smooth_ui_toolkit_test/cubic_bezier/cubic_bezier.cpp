d7dbf91f422f3480a194d35808c563b18c332336
Forairaaaaa <applesyqd@outlook.com>
2023-12-29 09:58:00 +0800

    add cubic bezier test

--- simu_project/smooth_ui_toolkit_test/cubic_bezier/cubic_bezier.cpp
+++ simu_project/smooth_ui_toolkit_test/cubic_bezier/cubic_bezier.cpp
@@ -17,16 +17,33 @@
 using namespace SMOOTH_UI_TK;
 
 
+// Mirro the y direction 
+int _mirro_y(int y)
+{
+    return HAL::GetCanvas()->height() - y;
+}
+
+
 void cubic_bezier_test()
 {
     spdlog::info("cubic bezier test");
 
+    int scale = 5;
+    int x_offset = (HAL::GetCanvas()->width() - 1024 / scale) / 2;
+    int y_offset = (HAL::GetCanvas()->height() - 1024 / scale) / 2;
 
-    spdlog::info("{} {} {} {}", 
-        EasingPath::easeInSine.getControlPoints().x, 
-        EasingPath::easeInSine.getControlPoints().y, 
-        EasingPath::easeInSine.getControlPoints().z, 
-        EasingPath::easeInSine.getControlPoints().w
-    );
-    
+    // Draw path curve 
+    HAL::GetCanvas()->fillScreen(TFT_BLACK);
+    HAL::GetCanvas()->drawRect(x_offset, _mirro_y(y_offset), 1024 / scale, -(1024 / scale), TFT_DARKGRAY);
+    for (int t = 0; t < 1024; t++)
+    {
+        // auto b = EasingPath::easeOutBack(t);
+        // auto b = EasingPath::easeInOutQuint(t);
+        auto b = EasingPath::easeInOutCubic(t);
+
+        spdlog::info("b({}) = {} scale: b({}) = {}", t, b, t / scale, b / scale);
+        HAL::GetCanvas()->drawPixel(t / scale + x_offset, _mirro_y(b / scale + y_offset), TFT_YELLOW);
+    }
+    HAL::CanvasUpdate();
+    HAL::Delay(5000);
 }
